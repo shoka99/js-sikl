ac70a3bbe31acac4612516f33716c15ee9856431
shoka99 <92096028+shoka99@users.noreply.github.com>
2021-11-18 00:45:11 +0500

    Update script4.js

---
+++ javascript/script4.js
@@ -0,0 +1,15 @@
+var sikl=+prompt("Son kiritib darajasini bilgilab bering Biz javobini chiqarib beramiz")
+var son=+prompt("Son kiriting")
+while(isNaN(son) || son==0){
+    son=+prompt("Siz son kiritmadingiz")
+}
+var daraja=+prompt("Darajani belgilang")
+while(isNaN(daraja) || daraja==0){
+    daraja=+prompt("Son kiriting")
+}
+javob=son
+for (let i = 1; i < daraja; i++) {
+    javob*=son
+    
+}
+alert("Siz "+son+" darajasi "+daraja+" kiritdingiz. javob:"+javob );
