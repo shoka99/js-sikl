696c1e742a5deb8a1ea97ad91cf1fac1160b8024
shoka99 <92096028+shoka99@users.noreply.github.com>
2021-11-18 00:46:09 +0500

    Update script2.js

--- javascript/script2.js
+++ javascript/script2.js
@@ -1,15 +1,24 @@
 var a = +prompt("Kettu o'rta qiymatni chiramiz")
 var num1 = +prompt("1-sonni kiriting")
+while(isNaN(num1) || num1==0){
+    num1 = +prompt("siz son kiritmadingiz")
+}
 var num2 = +prompt("2-sonni kiriting")
+while(isNaN(num2) || num2==0){
+    num2 = +prompt("siz son kiritmadingiz")
+}
 var num3 = +prompt("3-sonni kiriting")
+while(isNaN(num3) || num3==0){
+    num3 = +prompt("siz son kiritmadingiz")
+}
 
-if (num1<num2 && num2<num3 || num2<num3 && num2>num1) {
+if (!isNaN(num2) && num1<num2 && num2<num3 || num2<num3 && num2>num1) {
     alert("o'rta qiymat " + num2)
 } 
-else if (num2<num1 && num1<num3 || num3<num1 && num1<num2) {
+else if (!isNaN(num1) && num2<num1 && num1<num3 || num3<num1 && num1<num2) {
     alert("o'rta qiymat " + num1)
 }
-else if (num1<num3 && num3<num2 || num2<num3 && num3<num1) {
+else if (!isNaN(num3) && num1<num3 && num3<num2 || num2<num3 && num3<num1) {
     alert("o'rta qiymat " + num3)
 }
 else{
